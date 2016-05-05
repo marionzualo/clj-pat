305a3ea8383a45482f010e1a779f18b65e0dea34
Mário Nzualo <mario.nzualo@gmail.com>
2016-05-05 08:04:16 +0100

    Handle errors

--- src/clj_pat/core.clj
+++ src/clj_pat/core.clj
@@ -1,10 +1,20 @@
 (ns clj-pat.core
   (:require [clj-http.client :as client]
-            [cheshire.core :as json]))
+            [cheshire.core :as json])
+  (:use [slingshot.slingshot :only [try+]]))
 
-(defn postcode
-  [code]
+(defn get-body [response]
+  (json/parse-string (:body response)))
+
+(defn get-data [url]
+  (try+
+    (let [response (client/get url {:throw-entire-message? true})]
+      (get-body response))
+    (catch Object _
+      (let [body (get-body (:object &throw-context))]
+        {:error {:status (get body "code") :message (get body "error")}}))))
+
+(defn postcode [code]
   (let [parsed-code (clojure.string/replace code " " "")
-        url (str "http://uk-postcodes.com/postcode/" parsed-code ".json")
-        response (client/get url)
-        data (json/parse-string (:body response))] data))
+        url (str "http://uk-postcodes.com/postcode/" parsed-code ".json")]
+    (get-data url)))
--- test/clj_pat/core_test.clj
+++ test/clj_pat/core_test.clj
@@ -9,3 +9,8 @@
   (let [fake-body (json/generate-string {:postcode "KM21 4JR" :city "test City"})]
     (with-fake-routes {"http://uk-postcodes.com/postcode/KM214JR.json" (fn [request] {:status 200 :headers {} :body fake-body})}
       (is (= { "postcode" "KM21 4JR" "city" "test City"} (postcode "KM21 4JR"))))))
+
+(deftest test-postcode-error
+  (let [fake-body (json/generate-string {:code 404 :error "Postcode is not valid"})]
+    (with-fake-routes {"http://uk-postcodes.com/postcode/KM214JR.json" (fn [request] {:status 404 :headers {} :body fake-body})}
+      (is (= {:error {:status 404 :message "Postcode is not valid"} } (postcode "KM21 4JR"))))))
